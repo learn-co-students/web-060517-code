ba7e1b310a17f1fd32a5a0993d9dc2dc7ce5e808
Johann Kerr <johannkerr@gmail.com>
2017-06-28 12:04:07 -0400

    adding solution to forms

--- forms/app/controllers/users_controller.rb
+++ forms/app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
-  
+
+
+  #index
+
+  get '/users' do
+    @users = User.all
+    erb :'users/index.html'
+  end
+
+  #new
+  get '/users/new' do
+    erb :'users/new.html'
+  end
+
+  #show
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'users/show.html'
+  end
+
+
+  #create
+
+  post '/users' do
+    #user = User.create(params[:user])
+    user = User.new(params[:user])
+    if user.save
+      redirect "/users/#{user.id}"
+    else
+      redirect "/users/new"
+    end
+  end
+
+
+
 
 end
--- forms/config.ru
+++ forms/config.ru
@@ -1,4 +1,6 @@
 require_relative 'config/environment.rb'
 
+
+use DogsController
 use UsersController
 run ApplicationController
